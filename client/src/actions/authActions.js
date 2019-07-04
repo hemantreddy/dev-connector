c3f52393430680c77a7385f390d2c7ab75fd41c4
Hemant Reddy <hemantreddy22@gmail.com>
2019-07-04 15:25:52 +0530

    redux implementation

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,8 @@
+import { TEST_DISPATCH } from './types'
+//register user 
+export const registerUser = (userData) => {
+    return {
+        type : TEST_DISPATCH,
+        payload : userData
+    }
+};
