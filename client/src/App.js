eb80f90023a3e679c34f23e0bc9acd412d43a900
Hemant Reddy <hemantreddy22@gmail.com>
2019-07-08 14:28:24 +0530

    initiliased create profile page

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Dashboard from './components/dashboard/Dashboard'
+import CreateProfile from './components/create-profile/CreateProfile'
 
 import './App.css';
 
@@ -54,6 +55,12 @@ class App extends Component {
                 <PrivateRoute
                   exact path="/dashboard"
                   component={Dashboard}
+                />
+              </Switch>
+              <Switch>
+                <CreateProfile
+                  exact path="/create-profile"
+                  component={CreateProfile}
                 />
               </Switch>
             </div>
