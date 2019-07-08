0b8a8a57a3a27dd28c86f89abbb340b90f851d63
Hemant Reddy <hemantreddy22@gmail.com>
2019-07-08 19:47:05 +0530

    add education component

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -43,6 +43,17 @@ export const addExperience = (expData, history) => dispatch => {
             payload : err.response.data
         }));
 }
+
+//add education
+
+export const addEducation = (eduData, history) => dispatch => {
+    axios.post('profile/education', eduData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => dispatch({
+            type : GET_ERRORS,
+            payload : err.response.data
+        }));
+}
 //delete account and profile
 
 export const deleteAccount = () => dispatch => {
