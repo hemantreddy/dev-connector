7a80c2df1993a8df237858a1ef6a15581276015a
Hemant Reddy <hemantreddy22@gmail.com>
2019-07-08 14:14:04 +0530

    private dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import jwt_decode from 'jwt-decode'
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logOutUser } from './actions/authActions'
 import { clearCurrentProfile } from './actions/profileActions'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 import { Provider } from 'react-redux';
 import store from './store'
+
+import PrivateRoute from './components/common/PrivateRoute'
 
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
@@ -40,19 +42,24 @@ if(localStorage.jwtToken) {
 class App extends Component {
   render() {
     return (
-      <Provider store = {store}>
-      <Router>
-        <div className="App">
-          <Navbar/>
-          <Route exact path = "/" component = {Landing} />
-          <div className = "container">
-            <Route exact path = "/register" component = {Register}/>
-            <Route exact path = "/login" component = {Login}/>
-            <Route exact path = "/dashboard" component = {Dashboard}/>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Switch>
+                <PrivateRoute
+                  exact path="/dashboard"
+                  component={Dashboard}
+                />
+              </Switch>
+            </div>
+            <Footer />
           </div>
-          <Footer/>
-        </div>
-      </Router>
+        </Router>
       </Provider>
     );
   }
