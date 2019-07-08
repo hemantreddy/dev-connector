b6bfb291bc4b37b6b10aea739825bf8d68388fec
Hemant Reddy <hemantreddy22@gmail.com>
2019-07-08 14:48:34 +0530

    initliased input fields for data input on the profile page

---
+++ client/src/components/common/InputGroup.js
@@ -0,0 +1,49 @@
+import React from "react";
+import classnames from "classnames";
+import propTypes from "prop-types";
+
+const InputGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  icon,
+  type,
+  onChange
+}) => {
+  return (
+    <div className="input-group mb-3">
+        <div className = 'input-group-prepend'>
+            <span className = "input-group-text">
+                <i className = "icon"/>
+            </span>
+        </div>
+        <textarea
+            className={classnames("form-control form-control-lg", {
+            "is-invalid": error
+            })}
+            placeholder={placeholder}
+            name={name}
+            value={value}
+            onChange={onChange}
+        />
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+InputGroup.propTypes = {
+  name: propTypes.string.isRequired,
+  placeholder: propTypes.string,
+  value: propTypes.string.isRequired,
+  icon: propTypes.string,
+  type: propTypes.string.isRequired,
+  error: propTypes.string,
+  onChange: propTypes.func.isRequired
+};
+
+InputGroup.defaultProps = {
+    type : 'text'
+}
+
+export default InputGroup;
