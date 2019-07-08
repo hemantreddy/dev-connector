27555fcde8fb447e3282bd73b5dc457a21b911dc
Hemant Reddy <hemantreddy22@gmail.com>
2019-07-08 15:31:00 +0530

    complete profile page

--- client/src/components/common/InputGroup.js
+++ client/src/components/common/InputGroup.js
@@ -15,10 +15,10 @@ const InputGroup = ({
     <div className="input-group mb-3">
         <div className = 'input-group-prepend'>
             <span className = "input-group-text">
-                <i className = "icon"/>
+                <i className = {icon}/>
             </span>
         </div>
-        <textarea
+        <input
             className={classnames("form-control form-control-lg", {
             "is-invalid": error
             })}
